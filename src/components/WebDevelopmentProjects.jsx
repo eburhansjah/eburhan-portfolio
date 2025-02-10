d23f899ad163e09f7d983375eb43c0600e8c033c
eburhan <130926828+eburhansjah@users.noreply.github.com>
2025-02-10 15:30:34 -0500

    update web dev projects page

--- src/components/WebDevelopmentProjects.jsx
+++ src/components/WebDevelopmentProjects.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 
-const WebAppWrapper = styled.div`
+const WebDevWrapper = styled.div`
     border: limegreen 1px inset;
     padding: 20px;
     margin: 20px 15vw;
@@ -57,10 +57,10 @@ const Li = styled.li`
     margin: 10px 0;
 `;
 
-export default function WebAppProjects() {
+export default function WebDevelopmentProjects() {
     return(
-        <WebAppWrapper id="web-apps">
-            <h2>Web Application Development Projects</h2>
+        <WebDevWrapper id="web-development">
+            <h2>Web Development Projects</h2>
 
             <StyledNav>
 
@@ -73,144 +73,82 @@ export default function WebAppProjects() {
                             <StyledSpanRight><h3>Sep 2024 - Dec 2024</h3></StyledSpanRight>
                         </StyledProjectTitle>
                     </StyledLi>
-                </StyledUl>
 
-                <StyledUl>
-                    <StyledLi><strong>Open project repo: </strong>
-                        <StyledLink to="https://github.com/EC528-Fall-2024/autopilot-dashboard.git"
-                                    target="_blank" rel="noopener noreferrer">autopilot-dashboard-repo</StyledLink>
-                    </StyledLi>
+                    <StyledUl>
+                        <StyledLi><strong>Open project: </strong>
+                            <StyledLink to="https://prod-autopilot-dashboard-f3dc9e.apps.shift.nerc.mghpcc.org/"
+                                        target="_blank" rel="noopener noreferrer">autopilot-dashboard-open-shift-demo
+                            </StyledLink>
 
-                    <StyledLi><strong>Tech Stack: </strong>JavaScript, React, Kubernetes, Carbon Design System,
-                    Bash, Helm, Docker, Kubernetes, Podman, OpenShift, Quay, Keycloak, GitHub OAuth, GitHub Projects</StyledLi>
-
-                    <StyledLi><strong>About</strong></StyledLi>
-                    <StyledLi>
-                        Developing a fully functional UI dashboard for Autopilot which is a Kubernetes-native daemon
-                        that continuously monitors and evaluates GPUs, network and storage health, designed to detect
-                        and report infrastructure-level issues during lifetime of AI workloads. It is an open-resource
-                        project developed by IBM Research.<br/>
-                        <br/>
-                        As a scrum master and a developer in a team of 6, we are guided by mentors from IBM.
-                    </StyledLi>
-
-                    <StyledLi>
-                        <strong>Features</strong>
-                        <ul>
-                            <Li>Agile driven development methodology with applied vertical slicing techniques</Li>
-                        </ul>
-
-                        <StyledLi><strong><u>Login Page</u></strong></StyledLi>
-                        <ul>
-                            <Li>User authentication to the cluster is RBAC based,
-                                integrating Keycloak with GitHub OAuth, and grants users either admin or viewer access
-                                based on credentials</Li>
-                            <Li>Deployed to MOC/NERC using Keycloak and GitHub Oauth </Li>
-                        </ul>
-
-                        <StyledLi><strong><u>Monitoring Page</u></strong></StyledLi>
-                        <ul>
-                            <Li>Displaying real-time node status and health check information (DCGM level 3 diagnostics)
-                                from Autopilot’s labelling of the worker nodes (using the Kubernetes Watch API)</Li>
-                            <Li>Expandable boxes for displaying detailed information for each node</Li>
-                            <Li>Search box and filtering for quick search and selection on worker nodes</Li>
-                        </ul>
-
-                        <StyledLi><strong><u>Testing Page</u></strong></StyledLi>
-                        <ul>
-                            <Li>Enables administrators to run health checks on worker nodes by sending REST API
-                                requests to IBM Autopilot</Li>
-                            <Li>Drop down feature to select types of health checks</Li>
-                            <Li>Selection of group of nodes to run the tests on</Li>
-                            <Li>Displaying test results to user in an embedded terminal window</Li>
-                        </ul>
-                    </StyledLi>
-                </StyledUl>
-
-                <StyledUl>
-                    <StyledLi>
-                        <StyledProjectTitle>
-                            <h3>
-                                WeatherApp, Boston MA
-                            </h3>
-                            <StyledSpanRight><h3>July 2024</h3></StyledSpanRight>
-                        </StyledProjectTitle>
-                    </StyledLi>
-                </StyledUl>
-
-                <StyledUl>
-                    <StyledLi><strong>Open project repo: </strong>
-                        <StyledLink to="https://github.com/eburhansjah/WeatherApp.git"
-                                    target="_blank" rel="noopener noreferrer">weather-app-repo</StyledLink>
-                    </StyledLi>
-
-                    <StyledLi><strong>Tech Stack: </strong>React Native, Xcode, JavaScript, useSWR,
-                        axios, Open Weather Map API, GitHub
-                    </StyledLi>
-
-
-                    <StyledLi><strong>About</strong></StyledLi>
-
-                    <StyledLi>
-                        Creating a mobile application that allows the user to select one of the cities in the US, and
-                        displays its corresponding current weather information, as well as a 5-day forecast. It is a
-                        react
-                        native project that was built and developed with macOS and iOS simulator.
-                    </StyledLi>
-
-                    <StyledLi>
-                        <strong>Features</strong>
-
-                        <ul>
-                            <Li>Drop down menu to select other major cities in US</Li>
-                            <Li>Display of the app dims at night time</Li>
-                            <Li>Interactive switch that allows users to toggle unit of temperature between Celsius and
-                            Fahrenheit</Li>
-                        </ul>
-
-                        <StyledLi>Displaying current weather information:</StyledLi>
-
-                        <ul>
                             <ul>
-                                <Li>Weather icon</Li>
-                                <Li>Weather condition and description</Li>
-                                <Li>Current temperature</Li>
-                                <Li>Minimum and maximum temperature</Li>
-                                <Li>Feels like temperature</Li>
+                                <Li>
+                                    A temporary account with user permissions to the deployment link for everyone else
+                                    to view project. Access with the following credentials:
+                                </Li>
                             </ul>
-                        </ul>
 
-                        <StyledLi>Displaying 5-day forecast weather information:</StyledLi>
-                        <ul>The weather predicted for each day is according to the most prevalent condition on that day</ul>
-
-                        <ul>
                             <ul>
-                                <Li>Weather icon</Li>
-                                <Li>Weather description</Li>
-                                <Li>Minimum and maximum temperature</Li>
+                                <ul>
+                                    <Li>Username: autopilot-dashboard</Li>
+                                    <Li>Password: autopilot-dashboard</Li>
+                                </ul>
                             </ul>
-                        </ul>
+                        </StyledLi>
 
-                        <StyledLi>Dynamic animated backgrounds according to the current weather condition:</StyledLi>
+                        <StyledLi><strong>Open project repo: </strong>
+                            <StyledLink to="https://github.com/EC528-Fall-2024/autopilot-dashboard.git"
+                                        target="_blank" rel="noopener noreferrer">autopilot-dashboard-repo</StyledLink>
+                        </StyledLi>
 
-                        <ul>
+                        <StyledLi><strong>Tech Stack: </strong>JavaScript, React, Kubernetes, Carbon Design System,
+                        Bash, Helm, Docker, Kubernetes, Podman, OpenShift, Quay, Keycloak, GitHub OAuth, GitHub Projects
+                        </StyledLi>
+
+                        <StyledLi><strong>About</strong></StyledLi>
+                        <StyledLi>
+                            Developing a fully functional UI dashboard for Autopilot which is a Kubernetes-native daemon
+                            that continuously monitors and evaluates GPUs, network and storage health, designed to detect
+                            and report infrastructure-level issues during lifetime of AI workloads. It is an open-resource
+                            project developed by IBM Research.<br/>
+                            <br/>
+                            As a scrum master and a developer in a team of 6, we are guided by mentors from IBM.
+                        </StyledLi>
+
+                        <StyledLi>
+                            <strong>Features</strong>
                             <ul>
-                                <Li>Clouds</Li>
-                                <Li>Rain</Li>
-                                <Li>Drizzle</Li>
-                                <Li>Thunderstorm</Li>
-                                <Li>Snow</Li>
-                                <Li>Clear</Li>
+                                <Li>Agile driven development methodology with applied vertical slicing techniques</Li>
                             </ul>
-                        </ul>
 
-                    </StyledLi>
+                            <StyledLi><strong><u>Login Page</u></strong></StyledLi>
+                            <ul>
+                                <Li>User authentication to the cluster is RBAC based,
+                                    integrating Keycloak with GitHub OAuth, and grants users either admin or viewer access
+                                    based on credentials</Li>
+                                <Li>Deployed to MOC/NERC using Keycloak and GitHub Oauth </Li>
+                            </ul>
 
-                </StyledUl>
+                            <StyledLi><strong><u>Monitoring Page</u></strong></StyledLi>
+                            <ul>
+                                <Li>Displaying real-time node status and health check information (DCGM level 3 diagnostics)
+                                    from Autopilot’s labelling of the worker nodes (using the Kubernetes Watch API)</Li>
+                                <Li>Expandable boxes for displaying detailed information for each node</Li>
+                                <Li>Search box and filtering for quick search and selection on worker nodes</Li>
+                            </ul>
+
+                            <StyledLi><strong><u>Testing Page</u></strong></StyledLi>
+                            <ul>
+                                <Li>Enables administrators to run health checks on worker nodes by sending REST API
+                                    requests to IBM Autopilot</Li>
+                                <Li>Drop down feature to select types of health checks</Li>
+                                <Li>Selection of group of nodes to run the tests on</Li>
+                                <Li>Displaying test results to user in an embedded terminal window</Li>
+                            </ul>
+                        </StyledLi>
+                    </StyledUl>
 
                 <hr/>
 
-                <StyledUl>
                     <StyledLi>
                         <StyledProjectTitle>
                             <h3>
@@ -243,7 +181,7 @@ export default function WebAppProjects() {
                         </StyledLi>
                     </StyledUl>
 
-                    <hr/>
+                <hr/>
 
                     <StyledLi>
                         <StyledProjectTitle>
@@ -309,7 +247,7 @@ export default function WebAppProjects() {
                         </StyledLi>
                     </StyledUl>
 
-                    <hr/>
+                <hr/>
 
                     <StyledLi>
                         <StyledProjectTitle>
@@ -363,7 +301,7 @@ export default function WebAppProjects() {
                         </StyledLi>
                     </StyledUl>
 
-                    <hr/>
+                <hr/>
 
                     <StyledLi>
                         <StyledProjectTitle>
@@ -402,10 +340,55 @@ export default function WebAppProjects() {
                         </StyledLi>
                     </StyledUl>
 
+                <hr/>
+
+                    <StyledLi>
+                        <StyledProjectTitle>
+                            <h3>
+                                MyRecipes, Boston MA
+                            </h3>
+                            <StyledSpanRight><h3>Jun - Aug 2023</h3></StyledSpanRight>
+                        </StyledProjectTitle>
+                    </StyledLi>
+
+                    <StyledUl>
+                        <StyledLi><strong>Open project repo: </strong>
+                            <StyledLink to="https://github.com/eburhansjah/MyRecipes.git"
+                                        target="_blank" rel="noopener noreferrer">MyRecipes-repo</StyledLink>
+                        </StyledLi>
+
+                        <StyledLi><strong>Tech Stack: </strong>PostgreSQL, Lucidchart</StyledLi>
+
+                        <StyledLi><strong>About</strong></StyledLi>
+                        <StyledLi>
+                            A database for an app that saves and manages recipes across different platforms for
+                            the user’s personal usage. The app would be an all-in-one app for the user to manage
+                            recipes, and create meal plans. Aside from this, users would be able to look up the
+                            nutritional content of each recipe. There are two plans. Basic plans are free of charge,
+                            but are not free of advertisements. Premium plans would be free of advertisements.<br/>
+                            <br/>
+                            The reason for doing so is because I have been handwriting online recipes,
+                            manually organizing, and creating meal plans on my tablet.
+                            That is why I would like to learn how I could develop a database that simplifies this
+                            process and potentially assist users to plan their meals. Such a database would also be
+                            useful for users to plan their next grocery visit, ensuring that they have gotten a
+                            proper amount of ingredients to minimize food waste.
+                        </StyledLi>
+
+                        <StyledLi>
+                            <strong>Feature</strong>
+                            <ul>
+                                <Li>A history table that would enable users to edit the information they have
+                                    entered previously. Examples would be when users wish to have a different
+                                    recipe name, change serving size of the recipe, cook time, and many more</Li>
+                            </ul>
+                        </StyledLi>
+                    </StyledUl>
+
                 </StyledUl>
             </StyledNav>
 
-        </WebAppWrapper>
+        </WebDevWrapper>
 
     );
 }
