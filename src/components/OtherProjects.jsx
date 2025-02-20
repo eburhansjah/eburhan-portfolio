8a85e5fd632c02564dc35ac79c7ef411510ac0e7
eburhan <130926828+eburhansjah@users.noreply.github.com>
2025-02-20 14:05:24 -0500

    Update other projects

--- src/components/OtherProjects.jsx
+++ src/components/OtherProjects.jsx
@@ -64,43 +64,6 @@ export default function OtherProjects() {
 
             <StyledNav>
                 <StyledUl>
-
-                    <StyledLi>
-                        <StyledProjectTitle>
-                            <h3>
-                                Image Encoder, Boston MA
-                            </h3>
-                            <StyledSpanRight><h3>Sep - Dec 2024</h3></StyledSpanRight>
-                        </StyledProjectTitle>
-                    </StyledLi>
-
-                    <StyledUl>
-                        <StyledLi><strong>Open project repo: </strong>
-                            <StyledLink to="https://github.com/eburhansjah/ec504_ImageEncoder.git"
-                                        target="_blank" rel="noopener noreferrer">image-encoder-repo</StyledLink>
-                        </StyledLi>
-
-                        <StyledLi><strong>Tech Stack: </strong>C, CMake, AndroidStudio</StyledLi>
-
-                        <StyledLi><strong>About</strong></StyledLi>
-                        <StyledLi>
-                            This video encoder was designed as part of a final project for Advanced Data Structures
-                            and Algorithms class (EC504) at Boston University. It accepts a folder of JPEG images and
-                            encodes them into a simplified MPEG-1 file using functions we wrote ourselves (in a team of 4).
-                        </StyledLi>
-
-                        <StyledLi>
-                            <strong>Feature</strong>
-                            <ul>
-                                <Li>A history table that would enable users to edit the information they have
-                                    entered previously. Examples would be when users wish to have a different
-                                    recipe name, change serving size of the recipe, cook time, and many more</Li>
-                            </ul>
-                        </StyledLi>
-                    </StyledUl>
-
-                    <hr/>
-
                     <StyledLi>
                         <StyledProjectTitle>
                             <h3>
@@ -119,7 +82,6 @@ export default function OtherProjects() {
                         <StyledLi><strong>Tech Stack: </strong>Verilog, Xilinx Vivado (CAD tool),
                             FPGA (Digilent Nexys A7-100T: FPGA Trainer Board)
                         </StyledLi>
-
 
                         <StyledLi><strong>About</strong></StyledLi>
 
@@ -149,111 +111,35 @@ export default function OtherProjects() {
                             </ul>
                         </StyledLi>
                     </StyledUl>
+                </StyledUl>
 
-                    <hr/>
+                <hr/>
 
+                <StyledUl>
                     <StyledLi>
                         <StyledProjectTitle>
                             <h3>
-                                MyRecipes, Boston MA
+                                Chip-Seq to Study TF Binding Sites, Boston MA
                             </h3>
-                            <StyledSpanRight><h3>Jun - Aug 2023</h3></StyledSpanRight>
+                            <StyledSpanRight><h3>Sep - Dec 2022</h3></StyledSpanRight>
                         </StyledProjectTitle>
                     </StyledLi>
 
                     <StyledUl>
-                        <StyledLi><strong>Open project repo: </strong>
-                            <StyledLink to="https://github.com/eburhansjah/MyRecipes.git"
-                                        target="_blank" rel="noopener noreferrer">MyRecipes-repo</StyledLink>
-                        </StyledLi>
-
-                        <StyledLi><strong>Tech Stack: </strong>PostgreSQL, Lucidchart</StyledLi>
-
-                        <StyledLi><strong>About</strong></StyledLi>
-                        <StyledLi>
-                            A database for an app that saves and manages recipes across different platforms for
-                            the user’s personal usage. The app would be an all-in-one app for the user to manage
-                            recipes, and create meal plans. Aside from this, users would be able to look up the
-                            nutritional content of each recipe. There are two plans. Basic plans are free of charge,
-                            but are not free of advertisements. Premium plans would be free of advertisements.<br/>
-                            <br/>
-                            The reason for doing so is because I have been handwriting online recipes,
-                            manually organizing, and creating meal plans on my tablet.
-                            That is why I would like to learn how I could develop a database that simplifies this
-                            process and potentially assist users to plan their meals. Such a database would also be
-                            useful for users to plan their next grocery visit, ensuring that they have gotten a
-                            proper amount of ingredients to minimize food waste.
-                        </StyledLi>
-
-                        <StyledLi>
-                            <strong>Feature</strong>
-                            <ul>
-                                <Li>A history table that would enable users to edit the information they have
-                                    entered previously. Examples would be when users wish to have a different
-                                    recipe name, change serving size of the recipe, cook time, and many more</Li>
-                            </ul>
-                        </StyledLi>
+                        <StyledLi><strong>Tech Stack: </strong>Matlab</StyledLi>
                     </StyledUl>
-
-                    <hr/>
-
-                    <StyledLi>
-                        <StyledProjectTitle>
-                            <h3>
-                                Image Editing App, Boston MA
-                            </h3>
-                            <StyledSpanRight><h3>Jan - May 2023</h3></StyledSpanRight>
-                        </StyledProjectTitle>
-                    </StyledLi>
 
                     <StyledUl>
-                        <StyledLi><strong>Open project repo: </strong>
-                            <StyledLink to="https://github.com/eburhansjah/IFP.git"
-                                        target="_blank" rel="noopener noreferrer">Image-Editing-App-repo</StyledLink>
-                        </StyledLi>
-
-                        <StyledLi><strong>Tech Stack: </strong>Java, AndroidStudio, Gitlab</StyledLi>
-
-                        <StyledLi>
-                            <strong>About</strong>
-                        </StyledLi>
-                        <StyledLi>
-                            A user-friendly android app that modifies uploaded images.
-                            Images can be in the form of .png files or .text files.
-                            The app displays the image/modified image on the screen.
-                        </StyledLi>
-                        <StyledLi>
-                            <strong>Features</strong>
-                            <StyledLi>Design decisions:</StyledLi>
-                            <ul>
-                                <Li>Scrolling layout for a better user experience</Li>
-                                <Li>Separated the upload image and upload text features into two different buttons</Li>
-                                <Li>Displayed text based buttons for user-specified inputs</Li>
-                                <Li>Implemented toast for error checking</Li>
-                            </ul>
-
-                            <StyledLi>Image modification features:</StyledLi>
-                            <ul>
-                                <Li>Uploading stored image and image based on a text file</Li>
-                                <Li>Zero GB:</Li>
-                                <ul>
-                                    <Li>Recolors the uploaded image by zero-ing out all green and blue components
-                                        of pixels. As a result, the modified image will have different shades of
-                                        red, ranging from black to full red</Li>
-                                </ul>
-                                <Li>Resizing</Li>
-                                <Li>Rotating</Li>
-                                <Li>Blurring</Li>
-                                <Li>Grayscaling</Li>
-                                <Li>Recoloring:</Li>
-                                <ul>
-                                    <Li>Recolors the uploaded image based on user input</Li>
-                                </ul>
-                                <Li>Generating histogram on RGB content of the uploaded image</Li>
-                            </ul>
-                        </StyledLi>
-
+                        <StyledLi><strong>Wet lab: </strong>cell culture, molecular cloning, ChIP-Seq</StyledLi>
                     </StyledUl>
+
+                    <StyledLi><strong>About</strong></StyledLi>
+
+                    <StyledLi>
+                        Analyzed sequencing data of 2 novel transcription factors (TFs) experimentally and computationally
+                        in a team of 6. We applied ChIP-Seq to globally map and characterize the binding sites for
+                        <i> E. coli</i>: nac and iclR
+                    </StyledLi>
 
                 </StyledUl>
             </StyledNav>
